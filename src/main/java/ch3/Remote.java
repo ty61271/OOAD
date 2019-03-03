20b358bcbbe1256ea902009d34e2809d8d458685
ty61271 <40565834+ty61271@users.noreply.github.com>
2019-03-03 17:49:00 +0800

    ch3

---
+++ src/main/java/ch3/Remote.java
@@ -0,0 +1,19 @@
+package ch3;
+
+public class Remote {
+
+    private DogDoor door;
+
+    public Remote(DogDoor door) {
+        this.door = door;
+    }
+
+    public void pressButton() {
+        System.out.println("Pressing the remote control button...");
+        if (door.isOpen()) {
+            door.close();
+        } else {
+            door.open();
+        }
+    }
+}
